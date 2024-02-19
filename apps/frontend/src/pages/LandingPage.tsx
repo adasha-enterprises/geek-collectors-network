46e51cec778c0f13d6f24814f46ca4e893baeb99
toco <ttachibana@my.bcit.ca>
2024-02-19 00:32:48 -0800

    feat(frontend): touch up on landing page

--- apps/frontend/src/pages/LandingPage.tsx
+++ apps/frontend/src/pages/LandingPage.tsx
@@ -1,56 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Button, Image, VStack } from '@chakra-ui/react';
-
-function AuthButtons() {
-  /* Temporary styles for buttons; to be replaced with Chakra 'useStyleConfig'. */
-  const buttonStyles = {
-    height: '4em',
-    width: { base: '50%', md: '50%' },
-    fontSize: { base: 'lg', md: 'xl' },
-  };
-
-  return (
-    <VStack w={'100%'} spacing={4}>
-      <Link to="/signup">
-        <Button
-          h={buttonStyles.height}
-          w={buttonStyles.width}
-          fontSize={buttonStyles.fontSize}>
-            SIGN UP
-        </Button>
-      </Link>
-      <Link to="/register">
-        <Button
-          h={buttonStyles.height}
-          w={buttonStyles.width}
-          fontSize={buttonStyles.fontSize}>
-            LOGIN
-        </Button>
-      </Link>
-    </VStack>
-  );
-}
+import { Button, Image, VStack } from '@chakra-ui/react';
 
 function LandingPage() {
   return (
     <VStack
       spacing={4}
-      w="100vw"
-      h="100vh"
-      bg="brand.500"
-      align="center"
-      justify="center">
-
-      {/* GGR Logo -- Placeholder */}
-      <Box>
-        <Image
-          src={'https://via.placeholder.com/200'}
-          alt={'Company Logog Placeholder'}
-          boxSize={{ base: '200px', md: '300px' }}
-        />
-      </Box>
-      <AuthButtons />
+      bg="background"
+      w={'100vw'}
+      h={'100vh'}
+      justify={'center'}
+    >
+      {/* <Image
+        src="gcn_logo.svg"
+        alt="gcn_logo"
+      /> */}
+      <Button
+        colorScheme="brand"
+        w={'90%'}
+      >
+        <Link to="/register">
+          LOGIN
+        </Link>
+      </Button>
+      <Button
+        colorScheme="brand"
+        variant="outline"
+        w={'90%'}
+      >
+        <Link to="/signup">
+          SIGN UP
+        </Link>
+      </Button>
     </VStack>
   );
 }
