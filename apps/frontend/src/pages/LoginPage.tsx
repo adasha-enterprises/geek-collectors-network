81088ed2a3d6f562948da5055ec565992930fced
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-02-23 12:01:30 -0800

    fix(frontend): remove `PageLink`, whcih broke app

--- apps/frontend/src/pages/LoginPage.tsx
+++ apps/frontend/src/pages/LoginPage.tsx
@@ -19,7 +19,6 @@ function LoginPage({ formComponent }: LoginPageProps) {
       >
         <PageTitle title={'Login'} />
         {formComponent}
-        <PageLink text={"Don't have an account? Sign up!"} to={'/register'} />
       </VStack>
     </PageLayout>
   );
