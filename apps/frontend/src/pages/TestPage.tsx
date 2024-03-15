8fbbc2d5e035bf73fd401f7db62b038295dc6de9
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-03-15 16:43:45 -0700

    fix(frontend): update import, add prop

--- apps/frontend/src/pages/TestPage.tsx
+++ apps/frontend/src/pages/TestPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import FriendsList from './FriendsList';
-import PageLayout from './PageLayout';
+import PageLayout from '../components/PageLayout';
 
 function TestPage() {
   return (
-    <PageLayout>
+    <PageLayout showNavigation={false}>
       <FriendsList />
     </PageLayout>
   );
