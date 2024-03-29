51b349221e47833808ff90ca8986e15793e00f14
Terence Grigoruk <97200544+tgrigoruk@users.noreply.github.com>
2024-03-29 14:39:30 -0700

    feat(api): add endpoints to get items collections wishlists (#173)
    * feat: add ItemService outline
    
    * feat: add get user collection route
    
    * feat: add ItemService outline
    
    * feat: add get user collection route
    
    * feat: add get wishlist items route
    
    * refactor: use drizzle methods to get collection
    
    * feat: include tags with get collection & wishlist

--- apps/api/src/server/routes/v1.ts
+++ apps/api/src/server/routes/v1.ts
@@ -3,16 +3,19 @@ import express from 'express';
 import { use } from './utils';
 import { Resources } from '../services/Service';
 import { UserService } from '../services/UserService';
+import { ItemService } from '../services/ItemService';
 import { AuthService } from '../services/AuthService';
 
 import { authenticate } from '../middleware/Authenticate';
 
 export class Routes {
   private readonly userService: UserService;
+  private readonly itemService: ItemService;
   private readonly authService: AuthService;
 
   constructor(resources: Resources) {
     this.userService = new UserService(resources);
+    this.itemService = new ItemService(resources);
     this.authService = new AuthService(resources);
   }
 
@@ -37,6 +40,26 @@ export class Routes {
     router.get('/friendship/requests', authenticate, use((req, res) => this.userService.handleGetFriendRequests(req, res)));
     router.post('/friendship/:userId', authenticate, use((req, res) => this.userService.handleCreateFriendRequest(req, res)));
     router.patch('/friendship/:userId', authenticate, use((req, res) => this.userService.handleUpdateFriendRequest(req, res)));
+
+    // Item routes
+
+    // search for items
+    // router.get('/items', authenticate, use((req, res) => this.itemService.handleSearchItems(req, res)));
+    // item crud operations
+    // router.post('/item/', authenticate, use((req, res) => this.itemService.handleCreateItem(req, res)));
+    router.get('/item/:itemId', authenticate, use((req, res) => this.itemService.handleGetItem(req, res)));
+    // router.patch('/item/:itemId', authenticate, use((req, res) => this.itemService.handleUpdateItem(req, res)));
+    // router.delete('/item/:itemId', authenticate, use((req, res) => this.itemService.handleDeleteItem(req, res)));
+    // item tags
+    // router.post('/item/:itemId/:tagId?', authenticate, use((req, res) => this.itemService.handleAddItemTag(req, res)));
+    // router.delete('/item/:itemId/:tagId', authenticate, use((req, res) => this.itemService.handleRemoveItemTag(req, res)));
+
+    // User Collection Items routes
+    router.get('/user/:userId?/collection', authenticate, use((req, res) => this.itemService.handleGetUserCollection(req, res)));
+
+    // User Wishlist Items routes
+    router.get('/user/:userId?/wishlist', authenticate, use((req, res) => this.itemService.handleGetUserWishlist(req, res)));
+
 
     return router;
   }
---
+++ apps/api/src/server/services/ItemService.ts
@@ -0,0 +1,107 @@
+import express from 'express';
+import { eq, sql } from 'drizzle-orm';
+
+import { type Resources } from './Service';
+import { items, ItemsType, itemsToTags } from '../../models/schema';
+
+
+export class ItemController {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(private readonly resources: Resources) { }
+
+  public async getItem(id: number) {
+    const item = await this.resources.db.query.items.findFirst({
+      where: item_ => eq(item_.id, id),
+      with: { tags: { with: { tag: true } } },
+    });
+    if (item) {
+      item.tags = item.tags.map(tag => tag.tag.text);
+    }
+    return item;
+  }
+
+  public async getUserCollection(id: number) {
+    // TODO: if querying another user, omit items with isHidden = true
+    const results = await this.resources.db.query.itemsToUsersCollections.findMany({
+      where: item_ => eq(item_.userId, id),
+      with: { item: { with: { tags: { with: { tag: true } } } } },
+    });
+    if (results) {
+      return results.map(result => ({
+        ...result.item,
+        notes: result.notes,
+        tags: result.item.tags.map(tag => tag.tag.text),
+      }));
+    }
+    return results;
+  }
+
+
+  public async getUserWishlist(id: number) {
+    // TODO: if querying another user, omit items with isHidden = true
+    const results = await this.resources.db.query.itemsToUsersWishlists.findMany({
+      where: item_ => eq(item_.userId, id),
+      with: { item: { with: { tags: { with: { tag: true } } } } },
+    });
+    if (results) {
+      return results.map(result => ({
+        ...result.item,
+        notes: result.notes,
+        tags: result.item.tags.map(tag => tag.tag.text),
+      }));
+    }
+    return results;
+  }
+}
+
+export class ItemService {
+  private readonly controller: ItemController;
+
+  constructor(resources: Resources) {
+    this.controller = new ItemController(resources);
+  }
+
+  public async handleSearchItems(req: express.Request, res: express.Response) {}
+
+  public async handleCreateItem(req: express.Request, res: express.Response) {}
+
+  public async handleGetItem(req: express.Request, res: express.Response) {
+    try {
+      const itemId = parseInt(req.params.itemId, 10);
+      const item = await this.controller.getItem(itemId);
+      if (item) {
+        return item;
+      }
+      return new Error('Item not found');
+    } catch (err) {
+      return new Error('Internal Server Error');
+    }
+  }
+
+  public async handleUpdateItem(req: express.Request, res: express.Response) {}
+
+  public async handleDeleteItem(req: express.Request, res: express.Response) {}
+
+  public async handleAddItemTag(req: express.Request, res: express.Response) {}
+
+  public async handleRemoveItemTag(req: express.Request, res: express.Response) {}
+
+
+  public async handleGetUserCollection(req: express.Request, res: express.Response) {
+    try {
+      const userId = req.params.userId ? parseInt(req.params.userId, 10) : req.session.userId!;
+      return await this.controller.getUserCollection(userId);
+    } catch (err) {
+      return new Error('Internal Server Error');
+    }
+  }
+
+  public async handleGetUserWishlist(req: express.Request, res: express.Response) {
+    try {
+      const userId = req.params.userId ? parseInt(req.params.userId, 10) : req.session.userId!;
+      return await this.controller.getUserWishlist(userId);
+    } catch (err) {
+      return new Error('Internal Server Error');
+    }
+  }
+}
