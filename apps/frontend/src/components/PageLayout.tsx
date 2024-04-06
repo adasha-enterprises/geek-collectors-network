19bd59655814488b834f6cc053f0fdab6bbf34d4
Terence Grigoruk <terence.grigoruk@gmail.com>
2024-04-05 17:48:38 -0700

    feat(frontend): sticky top navbar

--- apps/frontend/src/components/PageLayout.tsx
+++ apps/frontend/src/components/PageLayout.tsx
@@ -10,9 +10,25 @@ type PageLayoutProps = {
 function PageLayout({ children, showNavigation }: PageLayoutProps) {
   return (
     <>
-      <Box w={'100vw'} h={'100vh'} bg={'background'}>
-        <Header showNavigation={showNavigation} />
-        {children}
+      <Box
+        w={'100vw'}
+        h={'100vh'}
+        bg={'background'}
+        position="relative"
+        overflow="hidden">
+        <Box
+          pos="fixed"
+          top={0}
+          left={0}
+          right={0}
+          zIndex="docked"
+          boxShadow="sm"
+        >
+          <Header showNavigation={showNavigation} />
+        </Box>
+        <Box overflowY="auto" h="full">
+          {children}
+        </Box>
       </Box>
     </>
   );
